242508f443c6e1a98d57f9e4ca76e15bb549c02f
Midka <me@midka.dev>
2022-08-19 19:26:28 +0300

    added something :shrug:

--- nixos/flake.nix
+++ nixos/flake.nix
@@ -1,6 +1,24 @@
 {
+  description = "Midka's Dev Server Dotfiles";
+
   inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
     home-manager.url = "github:nix-community/home-manager";
   };
-}
 
+  outputs = { self, nixpkgs, home-manager, nixos-hardware }: 
+    let 
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config = {
+          allowUnfree = true;
+        };
+      };
+    in
+    {
+      nixosConfigurations = {
+      };
+    };
+}
