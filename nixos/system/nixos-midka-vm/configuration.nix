444a8c0e4c45fc8c9640b18c0fbcf08b59abb3c3
Midka <me@midka.dev>
2022-08-25 17:27:15 +0300

    fixed configuration.nix

--- nixos/system/nixos-midka-vm/configuration.nix
+++ nixos/system/nixos-midka-vm/configuration.nix
@@ -103,7 +103,7 @@
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
   # accidentally delete configuration.nix.
-  system.copySystemConfiguration = true;
+  system.copySystemConfiguration = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
