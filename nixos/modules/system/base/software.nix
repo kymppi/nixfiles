2d6e08cf8cc883902aaa79c18742ec8845252f69
Midka <me@midka.dev>
2022-08-28 17:34:31 +0300

    feat(ssh): add more keys for midka

--- nixos/modules/system/base/software.nix
+++ nixos/modules/system/base/software.nix
@@ -2,6 +2,8 @@
 
 {
   environment.systemPackages = with pkgs; [
+    vim
+    neofetch
     neovim
     wget
     curl
--- nixos/system/vedenkeitin/configuration.nix
+++ nixos/system/vedenkeitin/configuration.nix
@@ -66,6 +66,11 @@
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.midka = {
     isNormalUser = true;
+    openssh.authorizedKeys.keys = [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC1h6h1R9O1sAk4mlJZYTEP27PuO9eEk6nZczOkQtgFwHpzhjMAnBpDD1WOJXVfX6XuWOoln9la5zAO5QCzUpqE9MdIMswlC/dMX4/J3PvwaHVNAzoq328q4d8TJGzS3INo//UE+hvaiS738hxs4hQ98F7IEQUdH0ZcBdZfpCnGQVxh3oXWcQlJMqikmAMw0iLauo1tAs3/aCjpQ2SHIsHkH4jjK107Y+gJU/ZMwvOgVTTIQDDeMw4sOs64cde0L+SvhhAKnHI4ZYG43c41gEfuTZyGUuFbtqGe+08kcLHI+W6u8z+2XlqV+L/9oCtDn5ZtEDJdf55zfQCo2J8L65XawycqXW5oZ6MQH+5xNm8AuX1J/dtyN0d8PII9QDC/k+1wZtamwngoobN+TzipY0NwxIsRUR73cOSix6/e04HP4YuZoGzGDLfGZqbDpUolb4ogACmqbfv1QxRtbwAeekYRut4GK7VG7Ssfx8QV9iCQlz1STZnhKej5G+ek1aeOTmX0Q9rm8CaQgajVStKhlQvLg8UjhA5+uPOljcZ3o7PGitIcyYjRXKQdC47VeqcI+RfLWuKmMsfpXm5i67awmKfMhl/1nX42a6BVZ3tY27lb3+iYjr7QbINBISvCRoj5Hyjo56NGktR0gu/nLGuLxV5Bj+uO2IvITgtchdHJ5cyexQ==
+"
+"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCu54FYP9cmUeaerZwBz1m+2TUw1Lo+DiR4Y9xYRNcuNJPp7fsgEBiahnAUjHApJ1LKoOtL/dHPPBfvVGUo90ViXoykLq20YgMkNSxlWxK1YeM1WCpH2/bNvl3f3RA/8yDsaQcftNozazBVHXLE9NT+yBEcOUIURnDI9yoBKQOn8LjkAkzuxqkn1oZ2+ghLP52gcUQDfdtvYcvIse42R259LEqeHZbOrStGMsB6tQSNcoNrUyzkmXLVwa56y6+pKSzFFcfeflEzPpA7SQLS/texOvmaBAeYhKKRQ+s/eHfwJg7KLY0c+Qes1UZevSXtB89WvVex86RepKu5kggQ/hrceRI5q3D9U7xeSQiW6hBUkk99kfuy3pFi1A+bdSacjZssosSAnP3gsHCb0Ec0/72ov+jfyLGoj4tROlhvNWhweIjFgzF2Sd5zlX1byv4GOBnqH2piTZStfo4idUVzD50yOeMIaC8ThT3i2k4TX8s1+jvj/pU4Ct6/kYYtuUjTOk0="
+    ];
     extraGroups = [ "wheel" "networkmanager" "docker" ]; # Enable ‘sudo’ for the user.
     packages = with pkgs; [];
     shell = pkgs.zsh;
@@ -81,29 +86,25 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    wget
-    neofetch
     sudo
-    git
-    neovim
-    bat
     home-manager
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
 
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
   services.openssh = {
 	enable = true;
+	permitRootLogin = "no";
+	passwordAuthentication = false;
 	listenAddresses = [{addr = "0.0.0.0"; port = 69;}];
   };
   # Open ports in the firewall.
